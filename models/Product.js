b325efe7bb35d08db3df5ba95c7c58d9c91074f3
Yamine Chowdhury <yaminechowdhury751@gmail.com>
2026-08-01 16:40:47 +0600

    First commit of OneShop Store

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  category: { type: String, default: 'General' },
-  image: { type: String, required: true },
-  description: { type: String }
+    title: { 
+        type: String, 
+        required: true 
+    },
+    details: { 
+        type: String 
+    },
+    price: { 
+        type: Number, 
+        required: true 
+    },
+    stock: { 
+        type: Number, 
+        default: 0 
+    },
+    sizes: [
+        { type: String } // S, M, L, XL, XXL ইত্যাদি
+    ],
+    img: { 
+        type: String 
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
