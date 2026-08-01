49583d9b096cc233222ef2031ba3d353df3597f3
Yamine Chowdhury <yaminechowdhury751@gmail.com>
2026-08-01 17:48:44 +0600

    Updated admin and index files

--- models/Product.js
+++ models/Product.js
@@ -1,27 +1,39 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    title: { 
-        type: String, 
-        required: true 
-    },
-    details: { 
-        type: String 
-    },
-    price: { 
-        type: Number, 
-        required: true 
-    },
-    stock: { 
-        type: Number, 
-        default: 0 
-    },
-    sizes: [
-        { type: String } // S, M, L, XL, XXL ইত্যাদি
-    ],
-    img: { 
-        type: String 
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String
+  },
+  details: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  stock: {
+    type: Number,
+    default: 0
+  },
+  sizes: [
+    { type: String }
+  ],
+  img: {
+    type: String
+  },
+  // নতুন যুক্ত করা অপশনসমূহ:
+  isFeatured: {
+    type: Boolean,
+    default: false
+  },
+  isHidden: {
+    type: Boolean,
+    default: false
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
